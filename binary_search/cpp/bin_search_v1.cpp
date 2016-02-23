fd818c08c63e6216a121f788f81b22e69b43031f
Stephen Doyle <stephendoyle75@gmail.com>
2016-02-23 21:45:21 +0000

    Adding recursive binary search implementation

--- binary_search/cpp/bin_search_v1.cpp
+++ binary_search/cpp/bin_search_v1.cpp
@@ -8,32 +8,45 @@
 
 bool
 binary_search_v1(const std::vector<int>::iterator begin,
-    const std::vector<int>::iterator end,
-    int target)
+                 const std::vector<int>::iterator end,
+                 int target)
 {
-  return false;
+    if(begin == end)
+        return false;
+    
+    // A recursive algorithm
+    std::vector<int>::iterator mid = begin + std::distance(begin, end)/2;
+    if(*mid == target) {
+        return true;
+    } else if(target < *mid) {
+        return binary_search_v1(begin, mid, target);
+    } else {
+        return binary_search_v1(mid, end, target);
+    }
+    
+    return false;
 }
 
 int main()
 {
-  const int data_size = 100;
-
-  // Fill a vector with some random values
-  std::vector<int> data(data_size);
-  std::generate(data.begin(), data.end(), std::rand);
-
-  // Remember the last element in the data set.
-  // This will be the target value to search for.
-  int val = data.back();
-
-  // Sort the vector
-  std::sort(data.begin(), data.end());
-
-  // Search for the target value
-  bool found = binary_search_v1(data.begin(), data.end(), val);
-
-  std::cout << val << (found ? " " : " not ") << "found" << std::endl;
-
-  return 0;
+    const int data_size = 100;
+    
+    // Fill a vector with some random values
+    std::vector<int> data(data_size);
+    std::generate(data.begin(), data.end(), std::rand);
+    
+    // Remember the last element in the data set.
+    // This will be the target value to search for.
+    int val = data.back();
+    
+    // Sort the vector
+    std::sort(data.begin(), data.end());
+    
+    // Search for the target value
+    bool found = binary_search_v1(data.begin(), data.end(), val);
+    
+    std::cout << val << (found ? " " : " not ") << "found" << std::endl;
+    
+    return 0;
 }
 
