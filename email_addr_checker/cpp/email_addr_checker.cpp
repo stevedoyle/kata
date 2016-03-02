2cc6516bc12d3ee25f69818d7357e45d2cdd1d45
Stephen Doyle <stephendoyle75@gmail.com>
2016-03-02 19:27:04 +0000

    Refactoring and adding c++ tests

--- email_addr_checker/cpp/email_addr_checker.cpp
+++ email_addr_checker/cpp/email_addr_checker.cpp
@@ -3,27 +3,47 @@
 #include <string>
 #include <regex>
 
+static int testsPassed = 0;
+static int testsFailed = 0;
+
+void printTestResultSummary()
+{
+    std::cout << testsPassed << " Tests Passed. " <<
+        testsFailed << " Tests Failed." << std::endl;
+}
 
 bool is_email_addr_valid(const std::string& addr)
 {
     const std::string email_regex =
-      "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+      "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$";
 
     return std::regex_match(addr, std::regex(email_regex));
 }
 
-int main ()
+void testEmailAddr(const std::string& addr, bool expected)
 {
-    const std::string testStr = "user@example.com";
-
-    if(is_email_addr_valid(testStr))
+    if(is_email_addr_valid(addr) != expected)
     {
-        std::cout << testStr << " is valid" << std::endl;
+        std::cout << "Fail: " << addr << std::endl;
+        testsFailed++;
     }
     else
     {
-        std::cout << testStr << " is not valid" << std::endl;
+        testsPassed++;
     }
+}
+
+int main ()
+{
+    testEmailAddr("user@example.com", true);
+    testEmailAddr("sample.user@example.com", true);
+    testEmailAddr("sample_user@example.com", true);
+    testEmailAddr("user@sub.example.com", true);
+    testEmailAddr("userexample.com", false);
+    testEmailAddr("sample user@example.com", false);
+    testEmailAddr("user@an_example.com", false);
+
+    printTestResultSummary();
 
     return 0;
 }
